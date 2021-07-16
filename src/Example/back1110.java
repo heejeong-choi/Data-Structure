487d6db5669491424f2444103fa69ad3d87093ca
heejeong-choi <jennachoi27@gmail.com>
2021-07-17 02:29:43 +0900

    Initial commit

---
+++ src/Example/back1110.java
@@ -0,0 +1,31 @@
+package Example;
+
+import java.util.Scanner;
+
+public class back1110 {
+
+    public static void main(String[] args) {
+
+        System.out.println("입력값: ");
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int setNum = num;
+        int count = 0;
+
+        while(true) {
+            int left = setNum/10;
+            int right = setNum%10;
+            setNum = right*10 + (left+right)%10;
+            count++;
+
+            if(num==setNum)   {
+                break;
+            }
+        }
+
+        System.out.println("길이: ");
+        System.out.println(count);
+    }
+}
