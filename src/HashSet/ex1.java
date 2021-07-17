eeca09fed090baca9733b2e3d4b8c0df864801a2
heejeong-choi <jennachoi27@gmail.com>
2021-07-17 19:46:24 +0900

    extra

---
+++ src/HashSet/ex1.java
@@ -0,0 +1,26 @@
+package HashSet;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class ex1 {
+    public static void main(String[] args) {
+        Object[] objArr = {"1", new Integer(1), "2", "2", "3", "3", "4", "4", "4"};
+        HashSet hashSet = new HashSet();
+
+        for(int i=0; i< objArr.length; i++) {
+            hashSet.add(objArr[i]);
+            // hashset에 objArr으 요소들을 저장
+        }
+
+        // hashSet에 저장된 요소를 출력
+        System.out.println(hashSet);
+
+        //HashSet에 저장된 요소들을 출력
+        Iterator iterator = hashSet.iterator();
+
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
---
+++ src/HashSet/ex2.java
@@ -0,0 +1,19 @@
+package HashSet;
+
+import java.util.*;
+
+// 로또번호 만들기
+public class ex2 {
+    public static void main(String[] args) {
+        HashSet hashSet = new HashSet();
+
+        for (int i=0; hashSet.size()<6; i++) {
+            int number = (int)(Math.random()*45)+1;
+            hashSet.add(new Integer(number));
+        }
+
+        LinkedList lls = new LinkedList(hashSet);
+        Collections.sort(lls); // 오름차순을 위한 collections
+        System.out.println(lls);
+    }
+}
