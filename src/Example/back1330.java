3f7c7267b97690974329e9008460dfa12cf27943
heejeong-choi <jennachoi27@gmail.com>
2021-07-26 15:11:49 +0900

    extra

---
+++ src/Example/back1330.java
@@ -0,0 +1,21 @@
+package Example;
+
+import java.util.Scanner;
+
+public class back1330 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        sc.close();
+
+        if(num1 > num2) {
+            System.out.println(">");
+        } else if(num1 == num2) {
+            System.out.println("=");
+        } else if(num1 < num2) {
+            System.out.println("<");
+        }
+    }
+}
