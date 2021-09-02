387214306c195428069c0f936b46f7f3fa661d9d
heejeong-choi <jennachoi27@gmail.com>
2021-09-02 16:06:38 +0900

    주어진 배열에서 최대 최소값 찾기

---
+++ src/Arrays/MinMaxProb.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class MinMaxProb {
+    public static void main(String[] args) {
+        int[] numbers = {17, 66, 93, 3, 82, 65, 51, 79, 20, 13, 43, 84}; // 배열 정의
+
+        int min = numbers[0]; //최솟값 초기화
+        int max = numbers[0]; //최댓값 초기화
+        int minPos = 0;
+        int maxPos = 0;
+
+        for(int i=1; i<numbers.length; i++) {
+            if(min > numbers[i]) {
+                min = numbers[i];
+                minPos = i+1; //위치
+            }
+            if(max < numbers[i]) {
+                max = numbers[i];
+                maxPos = i+1; //위치
+            }
+        }
+
+        System.out.println("Max value: " + max + ", and The location of max value in array " + maxPos);
+        System.out.println("Min value: " + min + ", and The location of min value in array " + minPos);
+    }
+}
